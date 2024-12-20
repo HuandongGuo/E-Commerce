ad2edd75215ba1269e55260570a11f2ed1d74ff6
huandong <huandong@buffalo.edu>
2024-12-19 19:35:39 -0600

    add railway url in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = 'django-insecure-n*qfw*%el8q5lf&m9y6u68(g+zbtu!k(aw8v*3)(844q^c5xt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['e-commerce-production-3e1c.up.railway.app', 'https://e-commerce-production-3e1c.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['e-commerce-production-3e1c.up.railway.app', 'https://e-commerce-production-3e1c.up.railway.app']
 
 # Application definition
 
