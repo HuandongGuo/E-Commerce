5030eca140d2527394b0a25080528c357ad85eb0
huandong <huandong@buffalo.edu>
2024-12-22 22:53:03 -0600

    added host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,8 +18,10 @@ SECRET_KEY = 'django-insecure-n*qfw*%el8q5lf&m9y6u68(g+zbtu!k(aw8v*3)(844q^c5xt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://urbanaurajewelry.com', 'urbanaurajewelry.com',  'e-commerce-production-3e1c.up.railway.app', 'https://e-commerce-production-3e1c.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://urbanaurajewelry.com', 'urbanaurajewelry.com', 'https://e-commerce-production-3e1c.up.railway.app']
+ALLOWED_HOSTS = ['https://urbanaurajewelry.com', 'urbanaurajewelry.com', 'e-commerce-production-3e1c.up.railway.app',
+                 'https://e-commerce-production-3e1c.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://urbanaurajewelry.com', 'https://e-commerce-production-3e1c'
+                                                        '.up.railway.app']
 
 # Application definition
 
@@ -36,7 +38,6 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
