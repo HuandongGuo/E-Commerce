ca629d9f6baa64a3ba31164ac073e7a19308b912
huandong <huandong@buffalo.edu>
2025-01-08 19:54:24 -0600

    fix staticfile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,10 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # White noise static stuff
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
